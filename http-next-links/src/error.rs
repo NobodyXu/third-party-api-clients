4e1127ef30cc186b774dc6ed0a41729b0cd27309
Jiahao XU <Jiahao_XU@outlook.com>
2023-02-14 12:30:35 +1100

    Improve err msg
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- http-next-links/src/error.rs
+++ http-next-links/src/error.rs
@@ -5,7 +5,7 @@ pub struct Error(pub(super) &'static str);
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Invalid link segment: {}", self.0)
+        write!(f, "Invalid http header link: {}", self.0)
     }
 }
 
